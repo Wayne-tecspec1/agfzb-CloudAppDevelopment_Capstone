419a68f53b114cc44f663e29698e23dbfcee3f69
Wayne <waynenmaiseni@gmail.com>
2023-05-17 05:33:05 -0400

    Commit Added Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,24 +4,48 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Create a Car Make model 
+class CarMake(models.Model):
+    cmname = models.CharField(null=False, max_length=100, default='Enter Make Name...')
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "Name: " + self.cmname + "," + \
+            "Description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+
+# Create a Car Model model 
+class CarModel(models.Model):
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "WAGON"
+    MAKE_TYPE = [
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "WAGON"),
+    ]
+    cname = models.CharField(null=False, max_length=100, default='Enter Model Name...')
+    dealerid = models.IntegerField()
+    cartype = models.CharField(
+        max_length=200,
+        choices=MAKE_TYPE,
+        default=SEDAN,
+    )
+    year = models.DateField(null=True)
+    carmakes = models.ManyToManyField(CarMake)
+
+    def __str__(self):
+        return self.cname
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer(models.Model):
+    cardealer_name = models.CharField(max_length=200, default="Enter Name...")
+    description = models.CharField(max_length=500)
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview(models.Model):
+    dealerreview_name = models.CharField(max_length=200, default="Enter Name...")
+    description = models.CharField(max_length=500)
