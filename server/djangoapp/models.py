f42eec9e83e4f2a7de941b738145b56cba6766f8
Wayne-tecspec1 <waynenmaiseni@gmail.com>
2023-05-18 02:13:45 -0400

    Commit updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -39,9 +39,28 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-class CarDealer(models.Model):
-    cardealer_name = models.CharField(max_length=200, default="Enter Name...")
-    description = models.CharField(max_length=500)
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -35,5 +35,4 @@ urlpatterns = [
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
- + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
